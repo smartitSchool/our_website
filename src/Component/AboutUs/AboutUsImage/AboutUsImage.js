6870dcecd55ccb477f95b62090bedd49c50452df
unknown <smartitlive@gmail.com>
2022-02-11 11:35:45 +0600

    about us page done

---
+++ src/Component/AboutUs/AboutUsImage/AboutUsImage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Image } from 'react-bootstrap';
+
+const AboutUsImage = () => {
+    return (
+        <Container>
+            <Image fluid={true} src="https://aie-internship.com/wp-content/uploads/Co-workers-working-in-start-up-office.jpg" />
+        </Container>
+    );
+};
+
+export default AboutUsImage;
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import AboutUsImage from '../../Component/AboutUs/AboutUsImage/AboutUsImage';
+import AboutUsText from '../../Component/AboutUs/AboutUsText/AboutUsText';
+import Footer from '../../Component/Footer/Footer';
+import Header from '../../Component/Header/Header';
 
 const About = () => {
     return (
         <div>
-            About us
+            <Header/>
+            <Container className='my-4'>
+            <h2 className='color-a'>About SIS</h2>
+            <hr />
+                <Row>
+                    <Col xs={12} md={6}>
+                        <AboutUsText/>
+                    </Col>
+                    <Col xs={12} md={6}>
+                        <AboutUsImage/>
+                    </Col>
+                </Row>
+            </Container>
+            <Footer/>
         </div>
     );
 };
